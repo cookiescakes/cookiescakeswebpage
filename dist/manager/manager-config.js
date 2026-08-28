09bd8a245b433f338303aae46f6d2643f4a1000e
Cookies Cakes Website <website@cookiescakes.local>
2026-08-28 20:23:45 +0100

    Fix dietary badge display and manager layout

---
+++ dist/manager/manager-config.js
@@ -0,0 +1,2 @@
+window.COOKIES_CAKES_MENU_WORKER_URL='https://cookiescakeswebpage.cookiescakes-uk.workers.dev';
+window.COOKIES_CAKES_MENU_DATA_URL='https://raw.githubusercontent.com/cookiescakes/cookiescakeswebpage/main/dist/menu-data.js';
