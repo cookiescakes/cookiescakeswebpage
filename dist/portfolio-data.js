21e654e89487a53ddaca8265d546a4d292a96da0
cookiescakes <cookiescakes.uk@gmail.com>
2026-08-29 22:49:55 +0100

    Update portfolio from Cookies Cakes manager

--- dist/portfolio-data.js
+++ dist/portfolio-data.js
@@ -1,5 +1,13 @@
 window.portfolioItems = [
   {
+    "id": "portfolio-1788040132971-uj9n1c",
+    "title": "Cookies Pie",
+    "caption": "",
+    "instagram": "",
+    "image": "portfolio-images/1788040171595-portfolio-1788040132971-uj9n1c-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
     "id": "portfolio-1788039410835-p23xyc",
     "title": "3 tiered fault line cake",
     "caption": "",
