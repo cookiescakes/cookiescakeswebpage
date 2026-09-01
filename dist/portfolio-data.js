dc5eac72d5e24de20e3747c119870547d0db1d9d
cookiescakes <cookiescakes.uk@gmail.com>
2026-09-01 20:41:19 +0100

    Update portfolio from Cookies Cakes manager

--- dist/portfolio-data.js
+++ dist/portfolio-data.js
@@ -1,5 +1,29 @@
 window.portfolioItems = [
   {
+    "id": "portfolio-1788291491819-v55h3v",
+    "title": "Double trouble birthday cake",
+    "caption": "An 8 inch round cake with 3 layers of chocolate chip cake and chocolate butter cream. Featuring a fault line design with sprinkles in black and purple themes.",
+    "instagram": "",
+    "image": "portfolio-images/1788291538205-portfolio-1788291491819-v55h3v-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
+    "id": "portfolio-1788291371976-z7rrx5",
+    "title": "21st birthday cake",
+    "caption": "A simple yet elegant cake. 6 inches. Layers of carrot cake and cream cheese frosting with some matching muffins.",
+    "instagram": "",
+    "image": "portfolio-images/1788291387818-portfolio-1788291371976-z7rrx5-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
+    "id": "portfolio-1788291172278-kp2h6t",
+    "title": "Yellow birthday cake",
+    "caption": "The theme was yellow 💛.  A carrot cake layered with cream cheese  frosting and a white chocolate drip. Decorated with edible sunflowers.",
+    "instagram": "",
+    "image": "portfolio-images/1788291253885-portfolio-1788291172278-kp2h6t-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
     "id": "portfolio-1788040132971-uj9n1c",
     "title": "Cookie Pie",
     "caption": "",
@@ -10,7 +34,7 @@ window.portfolioItems = [
   {
     "id": "portfolio-1788039410835-p23xyc",
     "title": "3 tiered fault line cake",
-    "caption": "",
+    "caption": "A 3 tired fault line cake with edible lace. Top layer is a vanilla sponge cake with  salted caramel and ganache filling. The middle is a clear layer. \nThe bottom layer is a lemon and blueberry cake with lemon curd and blueberry jam filling.",
     "instagram": "",
     "image": "portfolio-images/1788039501607-portfolio-1788039410835-p23xyc-portfolio-photo.jpg",
     "visible": true
