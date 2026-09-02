ee685f9843b77cbf098cfe1c645d0376f5ba28c2
cookiescakes <cookiescakes.uk@gmail.com>
2026-09-02 14:11:36 +0100

    Update portfolio from Cookies Cakes manager

--- dist/portfolio-data.js
+++ dist/portfolio-data.js
@@ -1,17 +1,21 @@
 window.portfolioItems = [
   {
     "id": "homepage-feature-cake-video",
-    "title": "Cake in the making",
+    "title": "Opening week cake shed",
     "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
     "caption": "",
     "instagram": "",
     "image": "portfolio-images/homepage-feature-cake.gif",
-    "displayAsHomepageFeature": true,
-    "visible": true
+    "visible": false
   },
   {
     "id": "portfolio-1788291491819-v55h3v",
     "title": "Double trouble birthday cake",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
     "caption": "An 8 inch round cake with 3 layers of chocolate chip cake and chocolate butter cream. Featuring a fault line design with sprinkles in black and purple themes.",
     "instagram": "",
     "image": "portfolio-images/1788291538205-portfolio-1788291491819-v55h3v-portfolio-photo.jpg",
@@ -20,6 +24,9 @@ window.portfolioItems = [
   {
     "id": "portfolio-1788291371976-z7rrx5",
     "title": "21st birthday cake",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
     "caption": "A simple yet elegant cake. 6 inches. Layers of carrot cake and cream cheese frosting with some matching muffins.",
     "instagram": "",
     "image": "portfolio-images/1788291387818-portfolio-1788291371976-z7rrx5-portfolio-photo.jpg",
@@ -28,6 +35,9 @@ window.portfolioItems = [
   {
     "id": "portfolio-1788291172278-kp2h6t",
     "title": "Yellow birthday cake",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
     "caption": "The theme was yellow 💛.  A carrot cake layered with cream cheese  frosting and a white chocolate drip. Decorated with edible sunflowers.",
     "instagram": "",
     "image": "portfolio-images/1788291253885-portfolio-1788291172278-kp2h6t-portfolio-photo.jpg",
@@ -36,6 +46,9 @@ window.portfolioItems = [
   {
     "id": "portfolio-1788040132971-uj9n1c",
     "title": "Cookie Pie",
+    "portfolioType": "treat",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
     "caption": "Biscoff cookie pie.",
     "instagram": "",
     "image": "portfolio-images/1788040171595-portfolio-1788040132971-uj9n1c-portfolio-photo.jpg",
@@ -44,10 +57,12 @@ window.portfolioItems = [
   {
     "id": "portfolio-1788039410835-p23xyc",
     "title": "3 tiered fault line cake",
+    "portfolioType": "cake",
+    "displayAsFeature": true,
+    "displayAsHomepageFeature": false,
     "caption": "A 3 tiered fault line cake with edible lace. Top layer is a vanilla sponge cake with  salted caramel and ganache filling. The middle is a clear decorative layer. \nThe bottom layer is a lemon and blueberry cake with lemon curd and blueberry jam filling.",
     "instagram": "",
     "image": "portfolio-images/1788039501607-portfolio-1788039410835-p23xyc-portfolio-photo.jpg",
-    "displayAsFeature": true,
     "visible": true
   }
 ];
