e5cf501fd8d0bad98f43d36b2da967aaf4061baa
cookiescakes <cookiescakes.uk@gmail.com>
2026-09-24 21:00:34 +0100

    Update portfolio from Cookies Cakes manager

--- dist/portfolio-data.js
+++ dist/portfolio-data.js
@@ -1,5 +1,82 @@
 window.portfolioItems = [
   {
+    "id": "portfolio-1790279926130-0cgzum",
+    "title": "Baby shower cake",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
+    "caption": "A smooth buttercream with fondant decorations.",
+    "instagram": "",
+    "image": "portfolio-images/1790279951520-portfolio-1790279926130-0cgzum-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
+    "id": "portfolio-1790279801408-923tvc",
+    "title": "10 inch birthday cake",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
+    "caption": "1st birthday cake. Filled with berry jam and vanilla butter cream.",
+    "instagram": "",
+    "image": "portfolio-images/1790279847432-portfolio-1790279801408-923tvc-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
+    "id": "portfolio-1790279626774-y6r96u",
+    "title": "40 and fuching fabulous",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
+    "caption": "A lovely elegant carrot cake with cream cheese buttercream. In the theme lilac and white.",
+    "instagram": "",
+    "image": "portfolio-images/1790279711855-portfolio-1790279626774-y6r96u-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
+    "id": "portfolio-1790279540682-vjt8ty",
+    "title": "Simple but elegant",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
+    "caption": "This was a vegan version. A delicious coffee and walnut cake with coffee buttercream.",
+    "instagram": "",
+    "image": "portfolio-images/1790279571483-portfolio-1790279540682-vjt8ty-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
+    "id": "portfolio-1790279378253-ozpp31",
+    "title": "Blue floral",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
+    "caption": "A yummy Victoria cake with raspberry jam. A navy blue with lotds of flowers in different methods.",
+    "instagram": "",
+    "image": "portfolio-images/1790279467561-portfolio-1790279378253-ozpp31-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
+    "id": "portfolio-1790279211966-8iieef",
+    "title": "Loaded Biscoff Cake",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
+    "caption": "Biscoff Biscoff Biscoff. A loaded biscoff cake. Biscoff cake. Biscoff filling. Biscoff buttercream. Biscoff drip. Biscoff Biscuits. Biscoff stuffed cookies.",
+    "instagram": "",
+    "image": "portfolio-images/1790279228147-portfolio-1790279211966-8iieef-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
+    "id": "portfolio-1790279027876-0fxks3",
+    "title": "XL cookie sheet cake",
+    "portfolioType": "cake",
+    "displayAsFeature": false,
+    "displayAsHomepageFeature": false,
+    "caption": "At a whooping 16 inches. A cookie cake in any flavour theme and buttercream/ ganache for those who don't want a cake cake. Can have more decorations if preferred. Was asked to keep this one simple.",
+    "instagram": "",
+    "image": "portfolio-images/1790279040625-portfolio-1790279027876-0fxks3-portfolio-photo.jpg",
+    "visible": true
+  },
+  {
     "id": "homepage-feature-cake-video",
     "title": "Cake in the making",
     "portfolioType": "cake",
@@ -58,7 +135,7 @@ window.portfolioItems = [
     "id": "portfolio-1788039410835-p23xyc",
     "title": "3 tiered fault line cake",
     "portfolioType": "cake",
-    "displayAsFeature": true,
+    "displayAsFeature": false,
     "displayAsHomepageFeature": false,
     "caption": "A 3 tiered fault line cake with edible lace. Top layer is a vanilla sponge cake with  salted caramel and ganache filling. The middle is a clear decorative layer. \nThe bottom layer is a lemon and blueberry cake with lemon curd and blueberry jam filling.",
     "instagram": "",
